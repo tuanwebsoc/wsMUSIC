d4f81029d0e9b274de8b47594523baa9e7fcbd3a
toan <neverback.back@gmail.com>
2014-03-11 22:34:03 +0700

    add helper_wsmusic & wsmusic.php in backend

--- component/admin/wsmusic.php
+++ component/admin/wsmusic.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ * @version 	0.0.1
+ * @copyright   Copyright (C) 2005 - 2014 WebSoc company
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die('Restricted access');
+JHtml::_('behavior.tabstate');
+
+// Access check: is this user allowed to access the backend of this component?
+if (!JFactory::getUser()->authorise('core.manage', 'com_wsmusic'))
+{
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
+JLoader::register('PortfolioHelper', dirname(__FILE__) . '/helpers/helper_wsmusic.php');
+
+$controller=JControllerLegacy::getInstance('wsmusic');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
